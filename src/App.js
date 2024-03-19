8c2ed8a13661dc9b008674edab68af1741955fb4
Roderick Shoolbraid <rdxenv@gmail.com>
2024-03-18 20:24:08 -0700

    calculator start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState('');
+
+  const handleButtonClick = (value) => {
+    setInput(input + value);
+  };
+
+  const calculateResult = () => {
+    try {
+      setInput(eval(input).toString());
+    } catch (error) {
+      setInput('Error');
+    }
+  };
+
+  const clearInput = () => {
+    setInput('');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+      <div className="App">
+      <div className="Brand">_HexCalc</div>
+      <div className="calculator">
+        <div className="display">{input || "0"}</div>
+        <div className="buttons">
+          <div className="row">
+            {'789/'.split('').map((symbol) => (
+              <button onClick={() => handleButtonClick(symbol)}>{symbol}</button>
+            ))}
+          </div>
+          <div className="row">
+            {'456*'.split('').map((symbol) => (
+              <button onClick={() => handleButtonClick(symbol)}>{symbol}</button>
+            ))}
+          </div>
+          <div className="row">
+            {'123-'.split('').map((symbol) => (
+              <button onClick={() => handleButtonClick(symbol)}>{symbol}</button>
+            ))}
+          </div>
+          <div className="row">
+            <button onClick={() => handleButtonClick('0')}>0</button>
+            <button onClick={calculateResult}>=</button>
+            <button onClick={clearInput}>C</button>
+            <button onClick={() => handleButtonClick('+')}>+</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
